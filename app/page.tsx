b52324a33073f7f01b67d40a4e926389fc5ebd1d
laguagu <49702497+laguagu@users.noreply.github.com>
2024-02-02 22:42:43 +0200

    feat: add tabs and button

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,42 @@
-export default function Home() {
-  return <div>Layout</div>;
+import { Button } from "@/components/ui/button";
+import { Separator } from "@/components/ui/separator";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+
+export default function MusicPage() {
+  return (
+    <div className="flex justify-around md:flex-row md:overflow-hidden mt-7">
+      <div className="flex">
+        <Tabs defaultValue="account" className="w-[400px]">
+          <TabsList className="bg-gray-100 ">
+            <TabsTrigger value="account">Account</TabsTrigger>
+            <TabsTrigger value="password">Password</TabsTrigger>
+            <TabsTrigger value="live" disabled>
+              Live
+            </TabsTrigger>
+          </TabsList>
+          <TabsContent value="account">
+            Make changes to your account here.
+          </TabsContent>
+          <TabsContent value="password">Change your password here.</TabsContent>
+        </Tabs>
+      </div>
+
+      <div className="flex justify-start">
+        
+        <Button variant="outline">Button</Button>
+      </div>
+      <div className="flex justify-end"></div>
+    </div>
+  );
 }
+
+// export default function Layout({ children }: { children: React.ReactNode }) {
+//   return (
+//     <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
+//       <div className="w-full flex-none md:w-64">
+//         <SideNav />
+//       </div>
+//       <div className="flex-grow p-6 md:overflow-y-auto md:p-12">{children}</div>
+//     </div>
+//   );
+// }
