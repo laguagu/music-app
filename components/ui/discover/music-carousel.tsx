15e526209f0f7119876b9caf3e55923c790421a7
laguagu <49702497+laguagu@users.noreply.github.com>
2024-02-08 18:34:21 +0200

    feat: add shadcn Resizable component

--- components/ui/discover/music-carousel.tsx
+++ components/ui/discover/music-carousel.tsx
@@ -1,3 +1,7 @@
+"use client"
+import * as React from "react"
+import Autoplay from "embla-carousel-autoplay"
+ 
 import { Card, CardContent } from "@/components/ui/card"
 import {
   Carousel,
@@ -7,21 +11,25 @@ import {
   CarouselPrevious,
 } from "@/components/ui/carousel"
  
-export default function CarouselSize() {
+export default function CarouselPlugin() {
+  const plugin = React.useRef(
+    Autoplay({ delay: 2000, stopOnInteraction: true })
+  )
+ 
   return (
     <Carousel
-      opts={{
-        align: "start",
-      }}
-      className="w-full max-w-sm"
+      plugins={[plugin.current]}
+      className="w-full max-w-xs mx-auto"
+      onMouseEnter={plugin.current.stop}
+      onMouseLeave={plugin.current.reset}
     >
       <CarouselContent>
         {Array.from({ length: 5 }).map((_, index) => (
-          <CarouselItem key={index} className="md:basis-1/2 lg:basis-2/3">
+          <CarouselItem key={index}>
             <div className="p-1">
               <Card>
                 <CardContent className="flex aspect-square items-center justify-center p-6">
-                  <span className="text-3xl font-semibold">{index + 1}</span>
+                  <span className="text-4xl font-semibold">{index + 1}</span>
                 </CardContent>
               </Card>
             </div>
